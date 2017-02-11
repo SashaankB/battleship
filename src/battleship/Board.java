49a8288b32230c17c500b3161728308da96533fc
natha <natha@35.40.127.219>
2017-02-10 20:06:11 -0500

    Updated Board

--- src/battleship/Board.java
+++ src/battleship/Board.java
@@ -1,5 +1,23 @@
 package battleship;
 
 public class Board {
-
+	
+	private final int B_SIZE = 10; 
+	private int[][] coord;
+	
+	public Board() {
+		
+		coord = new int[B_SIZE][B_SIZE];
+		
+	}
+	
+	public boolean hit(int x, int y) {
+		if ( coord[x][y] == 0) {
+			coord[x][y] = 1;
+			return true;
+		}
+		
+		return false;
+	}
+	
 }
