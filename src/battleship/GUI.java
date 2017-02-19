1fe1849868872345ceee974960640a1edc6cb129
Kellin <Kellin@Keunic>
2017-02-18 20:20:02 -0500

    Added all ship images and began implementing button features,

--- src/battleship/GUI.java
+++ src/battleship/GUI.java
@@ -1,13 +1,20 @@
 package battleship;
 
+import java.io.IOException;
+
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TitledPane;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.*;
 import javafx.stage.Stage;
+import sun.applet.Main;
  
 public class GUI extends Application {
     public static void main(String[] args) {
@@ -15,41 +22,52 @@ public class GUI extends Application {
     }
     
     
-    //just messing around with transparent buttons and background images
-    //image needs to be fixed and GUI modeled in scene builder
     @Override
-    public void start(Stage primaryStage) {
+    public void start(Stage primaryStage) throws IOException {
         primaryStage.setTitle("Battleship");
-        BackgroundImage BI = new BackgroundImage(new Image("battleshipBI.png", 1313, 750,false,true),
+        
+        BackgroundImage BI = new BackgroundImage(new Image("battleshipBackground.png", 1094, 625,false,true),
         		BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT,
                 BackgroundSize.DEFAULT);
-        
-        BorderPane border = new BorderPane();
-        border.setBackground(new Background(BI));
-        FlowPane flow = new FlowPane();
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        Button btn2 = new Button();
-        btn2.setText("Say 'Hello World'");
-        Button btn3 = new Button();
-        btn3.setText("Say 'Hello World'");
-        btn.setOpacity(0.1);
-        btn.setOnAction(new EventHandler<ActionEvent>() {
- 
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
-        
-        flow.getChildren().add(btn);
-        flow.getChildren().add(btn2);
-        flow.getChildren().add(btn3);
-        flow.setPrefWrapLength(170);
-        border.setLeft(flow);
-        primaryStage.setScene(new Scene(border, 1313, 750));
+
         primaryStage.setResizable(false);
         primaryStage.sizeToScene();
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(Main.class.getResource("/View.fxml"));
+        AnchorPane pane =  loader.load();
+        pane.setBackground(new Background(BI));
+        setShipButtons(pane);
+        Scene scene = new Scene(pane);
+        primaryStage.setScene(scene);
         primaryStage.show();
+    }
+    
+    private void setShipButtons(AnchorPane pane){
+    	ImageView viewShip1 = new ImageView(new Image(Main.class.getResourceAsStream("/ship1.png")));
+    	viewShip1.setLayoutX(65);
+    	viewShip1.setLayoutY(112);
+    	Button ship1 = new Button("");
+    	ship1.setLayoutX(65);
+    	ship1.setLayoutY(112);
+    	ship1.setPrefSize(71, 36);
+    	ship1.setOpacity(0.0);
+    	pane.getChildren().add(viewShip1);
+    	pane.getChildren().add(ship1);
+    	
+    	ship1.addEventHandler(MouseEvent.MOUSE_PRESSED,
+    		    new EventHandler<MouseEvent>() {
+    		        @Override public void handle(MouseEvent e) {
+    		        	viewShip1.setOpacity(0.5);
+    		        }
+    	});
+    	
+    	//TODO
+    	//Move the other 4 ship buttons/imageViews from View.fxml into setShipButtons
+    	//Using the PrefSize/Layout from View.fxml to set that info here
+    	//Gives us control when we want to have events happen on button press like you see above
+    	
+    	//TODO
+    	//Create a 10x10 of buttons on top of the grid for placing the ships, using for loop
+    	//Figure out how to cursor hover for buttons
     }
 }
