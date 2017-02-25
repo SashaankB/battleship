5c0a4397afd4314bd57f3968a53f0ba9d854d57b
Sean <Sean@35.40.125.236>
2017-02-24 20:30:16 -0500

    Added AI place ship components

--- src/battleship/AI.java
+++ src/battleship/AI.java
@@ -1,14 +1,63 @@
 package battleship;
 
+import java.util.Random;
+
 public class AI {	
 	//Keeps track of either hunt/target mode
 	private boolean hunt;
+
+	
+	
+	
 	
 	//AI Logic for setting the five ships
 	//Preferably random with ships not touching
 	public Ship[] setShips(){
+		
 		return null;
 		
+	}
+	
+//	public static int [] randomship(Ship ships){
+//		Random randomGenerator = new Random();
+//		int randx = randomGenerator.nextInt(10);
+//		System.out.println("Random x = " + randx);
+//		int randy = randomGenerator.nextInt(10);
+//		System.out.println("Random y = " + randy);
+//		
+//		int shipSize = ships.getShipSize();
+//		int valid = 11-shipSize;
+//		
+//		System.out.println("ship size" + shipSize);
+//		if(randx <= valid){
+//			System.out.println("valid ship placement");
+//		}
+//		else{
+//			System.out.println("no valid");
+//			
+//		}
+//		int [] start = new int[2];
+//		start[0] = randx;
+//		start[1] = randy;
+//		return start;
+//	}
+	public static int[] randomShip(int [] s){
+		Random randomGenerator = new Random();
+		int randx = randomGenerator.nextInt(10);
+		System.out.println("Random x = " + randx);
+		int randy = randomGenerator.nextInt(10);
+		System.out.println("Random y = " + randy);
+		
+		try{
+			
+		}
+		catch(Exception e){
+			
+		}
+		s = new int[2];
+		s[0] = randx;
+		s[1] = randy;
+		return s;
 	}
 	
 	/*Parameter is a board instance
