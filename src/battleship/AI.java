51ca25bceffafbcca13afbb182fe5b2aa3204231
natha <natha@10.0.0.8>
2017-02-25 22:10:46 -0500

    designed a very basic algorithm for the AI to pick a spot to shoot

--- src/battleship/AI.java
+++ src/battleship/AI.java
@@ -3,6 +3,12 @@ package battleship;
 public class AI {	
 	//Keeps track of either hunt/target mode
 	private boolean hunt;
+	private int currentRow;
+	
+	public AI() {
+		currentRow = 0;
+		hunt = true;
+	}
 	
 	//AI Logic for setting the five ships
 	//Preferably random with ships not touching
@@ -17,7 +23,24 @@ public class AI {
 	 * mode to make better decisions.
 	 */
 	public Point nextTurn(Board board){
-		return null;
-		
+		int row;
+		int col;
+		//checks to see if currentRow is out of bounds
+		if ( currentRow == 10){
+			row = 0;
+			currentRow = 0;
+		}
+		else
+			row = currentRow++;
+		//randomly selects a column in the given row
+		//if the row is even it selects an even column and vise versa
+		if ( row%2 == 0)
+			col = 0 + 2*(int)(Math.random()*((8-0)/2+1)); 
+		else
+			col = 1 + 2*(int)(Math.random()*((9-1)/2+1)); 
+		Point shot = new Point(col, row);
+		if ( !board.getPoint(shot))
+			return shot;
+		return nextTurn(board);
 	}
 }
